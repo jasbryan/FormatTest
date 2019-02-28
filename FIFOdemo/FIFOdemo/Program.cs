3efdde5c1fb41fa13246153145a924535869db8e
Jason Bryant <jasbryan@microsoft.com>
2019-02-28 14:43:19 -0800

    some stuff

--- FIFOdemo/FIFOdemo/Program.cs
+++ FIFOdemo/FIFOdemo/Program.cs
@@ -6,7 +6,35 @@ namespace FIFOdemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var CR1 = new CustomerRecord("blow", "jow", "324-234-1212");
+            var CR2 = new CustomerRecord("blow", "jolene", "123-345-5678");
+            var CR3 = new CustomerRecord("blow", "john", "324-234-2121");
+            var CR4 = new CustomerRecord("blow", "josephene", "987-789-6789");
+            var CR5 = new CustomerRecord("blow", "jason", "324-234-4545");
+
+            var myStack = new StackElement<CustomerRecord> { };
+
+            myStack.Push(CR1);
+            myStack.Push(CR2);
+            myStack.Push(CR3);
+            myStack.Push(CR4);
+            myStack.Push(CR5);
+
+            DisplayCusStack(myStack);
+            
+            
+           
+
+        }
+
+        private static void DisplayCusStack(StackElement<CustomerRecord> theCustomers)
+        {
+            Console.WriteLine($"Current Stack::");
+            foreach (var cus in theCustomers)
+            {
+                Console.WriteLine($"Here is the next Customer:{cus}");
+            }
+
         }
     }
 }
---
+++ FIFOdemo/FIFOdemo/StackElement.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FIFOdemo
+{
+    class StackElement<T> : IEnumerable<T>
+    {
+        private List<T> _ourList;
+
+        public int Count { get => _ourList.Count; }
+
+        public StackElement()
+        {
+            _ourList = new List<T> {};
+        }
+
+        public void Push(T obj)
+        {
+            _ourList.Add(obj);
+        }
+
+        public T Pop()
+        {
+            if (_ourList.Count > 0)
+            {
+                var temp = _ourList[_ourList.Count - 1];
+                _ourList.RemoveAt(_ourList.Count - 1);
+                return temp;
+            } else { throw new IndexOutOfRangeException("The list is empty"); }
+        }
+
+        public T Peek()
+        {
+            if(_ourList.Count > 0)
+            {
+                return _ourList[0];
+            }
+            else { throw new IndexOutOfRangeException("The list is empty"); }
+        }
+  
+        public IEnumerator<T> GetEnumerator()
+        {
+            if (_ourList.Count > 0)
+            {
+                var temp = new List<T> { };
+                for (int i = _ourList.Count - 1; i >= 0; i--)
+                {
+                    temp.Add(_ourList[i]);
+                }
+                return temp.GetEnumerator();
+            }
+            else { throw new IndexOutOfRangeException("The list is empty"); }
+
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

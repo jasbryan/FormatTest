6dfc0b802c65decc9f6d0beae690a2c7eb892f38
Jason Bryant <jasbryan@microsoft.com>
2019-03-12 15:32:50 -0700

    startEvents6

---
+++ Events6/Events6/CustomTimerClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Events6
+{
+    class CustomTimerClass
+    {
+
+        private bool _stopFlag = false;
+
+        public bool StopFlag { get => _stopFlag; set => _stopFlag = value; }
+
+        public delegate void TimerAlarmHandler();
+
+        public event TimerAlertHandler OnTimerAlarm;
+
+        public void run()
+        {
+            System.Threading.Thread.Sleep(1000);
+
+        }
+
+        public void RaiseTimerAlram()
+        {
+            if(true)
+            {
+                var tea = new TimerEventArgs();
+            }
+        }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Events6/Events6/TimerEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Events6
+{
+    class TimerEventArgs
+    {
+        private DateTime _time;
+
+        public String TimerDateTime { get => _time.ToString("HH:MM:SS"); }
+
+        public TimerEventArgs()
+        {
+            _time = DateTime.UtcNow;
+        }
+
+
+    }
+}
